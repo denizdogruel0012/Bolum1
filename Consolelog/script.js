0c4c6e47935326bd2a8bf988201af589892af691
denizdogruel00 <denizyazilm@gmail.com>
2025-11-19 09:07:25 +0300

    içine değişken tanımlama eklendi

---
+++ Consolelog/script.js
@@ -0,0 +1,6 @@
+console.log("Deniz")
+console.log(15)
+var x=20;
+var y=30;
+var toplam=x+y;
+console.log(toplam)
---
+++ Degisken-tanimlama/script.js
@@ -0,0 +1,4 @@
+let marka="Opel";
+let model="Corsa";
+let yil="2012";
+console.log(marka,model,yil)
