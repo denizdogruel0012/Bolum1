6050a0b3d7c07e8e262dd0788e712bd6ba0ebc2f
denizdogruel00 <denizyazilm@gmail.com>
2025-11-19 10:12:07 +0300

    string metodla eklendi

---
+++ string-methods/script.js
@@ -0,0 +1,14 @@
+/*
+
+javascript metodlarından splice belirli aralığı tutar
+substring kaçtan başlayıp kaç karaktter olmasını istiyorsak okadar yazdırılır
+
+replace değişiklik yapar
+
+ */
+
+
+let adSoyad="deniz doğruel";
+let parcala=adSoyad.slice(0,5);
+let substring=adSoyad.substring(0,5)
+console.log(substring)
