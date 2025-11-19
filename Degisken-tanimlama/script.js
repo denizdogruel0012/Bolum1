9b158a8255e825240108c14fd02533c58de64201
denizdogruel00 <denizyazilm@gmail.com>
2025-11-19 09:42:15 +0300

    string ifadeler eklendi

--- Degisken-tanimlama/script.js
+++ Degisken-tanimlama/script.js
@@ -2,3 +2,8 @@ let marka="Opel";
 let model="Corsa";
 let yil="2012";
 console.log(marka,model,yil)
+const pi=34.13;
+// pi=45.7;
+console.log(pi)
+yil="1998";
+console.log(yil)
---
+++ strings/script.js
@@ -0,0 +1,16 @@
+/*
+javascriptte string değişken tanımlarken çifttırnak kullanılır
+javascriptte string birleştirmede + işareti kullanılıor
+string ifadelerin uzunluğunda length kullanılır
+touppercase büyük harfe çevirir
+tolowercase küçük harfe çevirir
+indexof indexi gösterir
+*/
+let ad="deniz";
+let soyad="doğruel";
+let tamAd=ad+" "+soyad;
+console.log(tamAd)
+console.log(tamAd[0])
+console.log(tamAd.length)
+console.log(tamAd.toUpperCase());
+console.log(tamAd.toLowerCase());
